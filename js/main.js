b7d88b7868e4db1226e2b513b8bbdad930e766a7
Sean Boots <sean.boots@gmail.com>
2025-11-26 10:57:43 -0700

    Auto-generated feedback form query string

--- js/main.js
+++ js/main.js
@@ -15,3 +15,15 @@ document.querySelectorAll('[data-bs-toggle="popover"]')
   .forEach(popover => {
     new bootstrap.Popover(popover)
   })
+
+document.querySelectorAll('.yg-submit-feedback-link')
+  .forEach(link => {
+    const feedbackQueryString = `onlineService=${encodeURIComponent(document.title)}&url_referrer=${encodeURIComponent(window.location)}`
+    if (link.href.includes("?")) {
+      // In this case there's already a query string, so append with & in between
+      link.href = link.href + "&" + feedbackQueryString
+    } else {
+      // In this case, there isn't a query string yet, so append with a ? in between
+      link.href = link.href + "?" + feedbackQueryString
+    }
+  })
