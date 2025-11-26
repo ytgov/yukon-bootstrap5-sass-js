435dfd6d17b79a01dfd16dfca3a18c1407138853
Sean Boots <sean.boots@gmail.com>
2025-11-26 11:09:54 -0700

    Updated Javascript function to pre-set the onlineService name if it is not already set

--- js/main.js
+++ js/main.js
@@ -18,12 +18,22 @@ document.querySelectorAll('[data-bs-toggle="popover"]')
 
 document.querySelectorAll('.yg-submit-feedback-link')
   .forEach(link => {
-    const feedbackQueryString = `onlineService=${encodeURIComponent(document.title)}&url_referrer=${encodeURIComponent(window.location)}`
+    let feedbackQueryString = "";
+    // Check if an onlineService is already specified
+    if (link.href.includes("onlineService")) {
+      // If so, just provide the current URL
+      feedbackQueryString = `url_referrer=${encodeURIComponent(window.location)}`
+    } else {
+      // If not, then also provide the current page title as the onlineService name
+      feedbackQueryString = `onlineService=${encodeURIComponent(document.title)}&url_referrer=${encodeURIComponent(window.location)}`
+    }
+
+    // Check if there's an existing query string
     if (link.href.includes("?")) {
       // In this case there's already a query string, so append with & in between
-      link.href = link.href + "&" + feedbackQueryString
+      link.href = link.href + "&" + feedbackQueryString;
     } else {
       // In this case, there isn't a query string yet, so append with a ? in between
-      link.href = link.href + "?" + feedbackQueryString
+      link.href = link.href + "?" + feedbackQueryString;
     }
   })
